7eb8a138469837c4533378ba708971c011e7439b
Chetan Rautela <chetanrautela20@gmail.com>
2024-06-10 15:34:31 +0530

    added Undo in command Invoker

--- Assets/Scripts/Commands/CommandInvoker.cs
+++ Assets/Scripts/Commands/CommandInvoker.cs
@@ -1,3 +1,4 @@
+using Command.Main;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,12 +6,31 @@ using UnityEngine;
 public class CommandInvoker 
 {
     private Stack<ICommand> commandRegistry = new Stack<ICommand>();
-    
+   
+    private bool CommandBelongsToActivePlayer()
+    {
+        if((commandRegistry.Peek() as UnitCommand).commandData.actorPlayerID == GameService.Instance.PlayerService.ActivePlayerID)
+        {
+            return true;
+        }
+        else return false;
+    }
+    private bool registryEmpty() 
+    {
+        if (commandRegistry.Count == 0)
+            return true;
+        else return false;
+    }
     public void ExecuteCommand(ICommand commandToExecute)=>commandToExecute.Execute();    
     public void RegisterCommand(ICommand commandToRegister)=>commandRegistry.Push(commandToRegister);    
     public void ProcessCommand(ICommand commandToProcess)
     {
         ExecuteCommand(commandToProcess);
         RegisterCommand(commandToProcess);
+    }
+    public void Undo()
+    {
+        if(!registryEmpty() && CommandBelongsToActivePlayer())
+        commandRegistry.Pop().Undo();
     }
 }
